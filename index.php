2aa91dc1adbb03d69b9126d676d1df29ee7e3321
EduardoLazaroo <eduardolazaroso@hotmail.com>
2021-11-25 15:17:36 -0300

    suba

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atletica</title>
+</head>
+<body>
+    <header>
+        <div class="logo">Logo</div><!--logo-->
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+        <nav class="Mobile">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="banner-principal">
+
+    </section><!--banner principal-->
+</body>
+</html>
