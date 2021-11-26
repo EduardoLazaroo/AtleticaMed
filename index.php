2f66726e517bbe15d298c914dbddec1837f38e3b
EduardoLazaroo <eduardolazaroso@hotmail.com>
2021-11-26 11:42:10 -0300

    suba

--- index.php
+++ index.php
@@ -8,26 +8,80 @@
 </head>
 <body>
     <header>
+        <div class="w50">
         <div class="logo">Logo</div><!--logo-->
-        <nav class="desktop">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav>
-        <nav class="Mobile">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav>
+            <nav class="desktop">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="Mobile">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+        </div><!--w50-->
     </header>
     <section class="banner-principal">
 
     </section><!--banner principal-->
+    <section class="descricao-autor">
+    <div class="center">
+        <div class="w50">
+            <h2>Atletica medicina</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit unde corrupti molestias, 
+            nemo voluptates itaque, beatae eius rerum molestiae repellendus culpa sunt optio labore ea 
+            sed laboriosam libero atque? Facere.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit unde corrupti molestias, 
+            nemo voluptates itaque, beatae eius rerum molestiae repellendus culpa sunt optio labore ea 
+            sed laboriosam libero atque? Facere.</p>
+        </div><!--w50-->
+        <div class="w50">
+            <img src="images/fotoback.jpg">
+        </div><!--w50-->
+        </div><!--center-->
+    </section><!--descrição autor-->
+    <section class="especialidades">
+        <h2 class="title">Especialidades</h2>
+        <div class="center">
+            <div class="box-especialidades">
+                <h3>icone</h3>
+                <h3>css</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum explicabo laborum 
+                quia velit earum ab, suscipit, qui nam, in et ex sapiente consequatur sed. Corrupti 
+                harum laboriosam atque nam totam.</p>
+            </div><!--box-especialidades-->
+            <div class="box-especialidades">
+                <h3>icone2</h3>
+                <h3>css2</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum explicabo laborum 
+                quia velit earum ab, suscipit, qui nam, in et ex sapiente consequatur sed. Corrupti 
+                harum laboriosam atque nam totam.</p>
+            </div><!--box-especialidades-->
+            <div class="box-especialidades">
+                <h3>icone3</h3>
+                <h3>css3</h3>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum explicabo laborum 
+                quia velit earum ab, suscipit, qui nam, in et ex sapiente consequatur sed. Corrupti 
+                harum laboriosam atque nam totam.</p>
+            </div><!--box-especialidades-->
+        </div><!--center-->
+    </section><!--especialidades-->
+
+    <section class="extras">
+       
+        <div class="center">
+            <div class="w50">
+                <h2 class="title">Depoimentos</h2>
+            </div><!--w50-->
+        </div><!--center-->
+
+    </section><!--extras-->
 </body>
 </html>
