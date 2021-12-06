6e2da2ec2996e72614f53a95674a0608c88c94fb
EduardoLazaroo <eduardolazaroso@hotmail.com>
2021-12-06 14:15:33 -0300

    fluindo css

--- index.php
+++ index.php
@@ -4,36 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="estilo/style.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/44147d9e89.js" crossorigin="anonymous"></script><!--icon-->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"><!--font-->
     <title>Atletica</title>
 </head>
 <body>
     <header>
-        <div class="w50">
-        <div class="logo">Logo</div><!--logo-->
-            <nav class="desktop">
+        <div class="center">
+        <div class="logo left">Logo</div><!--logo-->
+            <nav class="desktop right">
                 <ul>
                     <li><a href="">Home</a></li>
-                    <li><a href="">Sobre</a></li>
-                    <li><a href="">Serviços</a></li>
-                    <li><a href="">Contato</a></li>
+                    <li><a href="">Eventos</a></li>
+                    <li><a href="">Modalidades</a></li>
+                    <li><a href="">Lojinha</a></li>
+                    <li><a href="">Gestão</a></li>
+                    <li><a href="">Hinos</a></li>
+                    <li><a href="">Parcerias</a></li>
                 </ul>
             </nav>
-            <nav class="Mobile">
+            <nav class="mobile right">
                 <ul>
                     <li><a href="">Home</a></li>
-                    <li><a href="">Sobre</a></li>
-                    <li><a href="">Serviços</a></li>
-                    <li><a href="">Contato</a></li>
+                    <li><a href="">Eventos</a></li>
+                    <li><a href="">Modalidades</a></li>
+                    <li><a href="">Lojinha</a></li>
+                    <li><a href="">Gestão</a></li>
+                    <li><a href="">Hinos</a></li>
+                    <li><a href="">Parcerias</a></li>
                 </ul>
             </nav>
-        </div><!--w50-->
+            <div class="clear"></div>
+        </div><!--center-->
     </header>
     <section class="banner-principal">
 
     </section><!--banner principal-->
     <section class="descricao-autor">
     <div class="center">
-        <div class="w50">
+        <div class="w50 left">
             <h2>Atletica medicina</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit unde corrupti molestias, 
             nemo voluptates itaque, beatae eius rerum molestiae repellendus culpa sunt optio labore ea 
@@ -42,29 +52,30 @@
             nemo voluptates itaque, beatae eius rerum molestiae repellendus culpa sunt optio labore ea 
             sed laboriosam libero atque? Facere.</p>
         </div><!--w50-->
-        <div class="w50">
+        <div class="w50 left">
             <img src="images/fotoback.jpg">
         </div><!--w50-->
+        <div class="clear"></div>
         </div><!--center-->
     </section><!--descrição autor-->
     <section class="especialidades">
-        <h2 class="title">Especialidades</h2>
         <div class="center">
-            <div class="box-especialidades">
+        <h2 class="title">Especialidades</h2>
+            <div class="w33 left box-especialidades">
                 <h3>icone</h3>
                 <h3>css</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum explicabo laborum 
                 quia velit earum ab, suscipit, qui nam, in et ex sapiente consequatur sed. Corrupti 
                 harum laboriosam atque nam totam.</p>
             </div><!--box-especialidades-->
-            <div class="box-especialidades">
-                <h3>icone2</h3>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fab fa-accessible-icon"></i></h3>
                 <h3>css2</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum explicabo laborum 
                 quia velit earum ab, suscipit, qui nam, in et ex sapiente consequatur sed. Corrupti 
                 harum laboriosam atque nam totam.</p>
             </div><!--box-especialidades-->
-            <div class="box-especialidades">
+            <div class="w33 left box-especialidades">
                 <h3>icone3</h3>
                 <h3>css3</h3>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum explicabo laborum 
@@ -77,7 +88,7 @@
     <section class="extras">
        
         <div class="center">
-            <div class="w50">
+            <div class="w50 left">
                 <h2 class="title">Depoimentos</h2>
                 <div class="depoimento-single">
                 <p class="depoimento-descricao">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti harum eaque 
@@ -98,7 +109,8 @@
                 <p class="class-autor">Lorem ipsum</p>
             </div><!--depoimento-singele-->
             </div><!--w50-->
-            <div class="w50">
+
+            <div class="w50 left">
                 <h2 class="title">serviços</h2>
                 <div class="servicos">
                 <ul>
@@ -114,6 +126,7 @@
                 </ul>
                 </div><!--servicos-->
             </div><!--w50-->
+            <div class="clear"></div>
         </div><!--center-->
     </section><!--extras-->
     <footer>
