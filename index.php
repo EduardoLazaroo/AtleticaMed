9c02a4c461885d5394f3cf5ecce5af1cd3fd6735
EduardoLazaroo <eduardolazaroso@hotmail.com>
2021-12-07 14:22:37 -0300

    css fluindo

--- index.php
+++ index.php
@@ -25,6 +25,9 @@
                 </ul>
             </nav>
             <nav class="mobile right">
+                <div class="botao-menu-mobile">
+                <i class="fas fa-bars"></i>
+                </div>
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Eventos</a></li>
@@ -42,12 +45,13 @@
     <section class="banner-principal">
         <div class="overlay"></div>
         <div class="center">
-            <form>
-                <div class="unimar">
+            <form  class="unimar">
+                <div>
                     <h2>Atlética</h2>
                     <h2>Medicina</h2>
                     <h2>Unimar</h2>
-                    <h3>a.a.a.a.n</h3>
+                    <h3>a . a . a . a . n</h3>
+                    <p>Associação Atlética Acadêmica Akira Nakadaira</p>
                 </div>
             </form>
         </div><!--center-->
@@ -62,16 +66,25 @@
     <section class="descricao-autor">
     <div class="center">
         <div class="w50 left">
-            <h2>Atletica medicina</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit unde corrupti molestias, 
-            nemo voluptates itaque, beatae eius rerum molestiae repellendus culpa sunt optio labore ea 
-            sed laboriosam libero atque? Facere.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit unde corrupti molestias, 
-            nemo voluptates itaque, beatae eius rerum molestiae repellendus culpa sunt optio labore ea 
-            sed laboriosam libero atque? Facere.</p>
+            <h2>O QUE É A A.A.A.A.N. ?</h2>
+            <p>A Associação Atlética Acadêmica Akira Nakadaira – AAAAN - é uma instituição que 
+                visa inserir o estudante de Medicina em atividades que possam promover a 
+                integração em áreas que estimulem a vivência fora do meio acadêmico. Tem como 
+                objetivos: a promoção e participação em eventos esportivos; o incentivo à prática 
+                esportiva através de treinos semanais; </p>
+            <p>Incentivo cultural com o desenvolvimento do grupo de percussão BATUKATUBRE e 
+                realização da festividade para recepção dos calouros, entre outros eventos 
+                para os quais somos convidados. Com a responsabilidade de representar a Faculdade 
+                de Medicina e, até mesmo, o nome da própria Universidade de Marília pelos eventos 
+                promovidos e participantes, a AAAAN busca sempre seriedade, compromisso e 
+                responsabilidade em suas ações.</p>
+            <p>Temos em mente que os projetos liderados pela AAAAN mostram que a Faculdade de Medicina 
+                da UNIMAR busca reconhecimento tanto dentro quanto fora do meio acadêmico. 
+                O nome representativo da nossa atlética é uma 
+                homenagem ao Dr. Akira Nakadaira, diretor evice-diretor da Faculdade de Medicina da UNIMAR.</p>
         </div><!--w50-->
         <div class="w50 left">
-            <img src="images/na.jpg">
+            <img class="right" src="images/na.jpg">
         </div><!--w50-->
         <div class="clear"></div>
         </div><!--center-->
