aca70af4cb35088e40908058ee343be1a45eeca6
EduardoLazaroo <eduardolazaroso@hotmail.com>
2021-12-17 18:05:45 -0300

    suba

---
+++ pages/header.php
@@ -0,0 +1,35 @@
+<head>
+ <!-- Style -->
+ <link href="estilo/style.css" rel="stylesheet">
+    
+</head>
+
+<header id="header" class="header" >
+    <div class="logo left">Logo</div>
+        <nav class="desktop right">
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#eventos">Eventos</a></li>
+                <li><a href="#modalidades">Modalidades</a></li>
+                <li><a href="#lojinha">Lojinha</a></li>
+                <li><a href="#Hinos">Hinos</a></li>
+                <li><a href="#parcerias">Parcerias</a></li>
+            </ul>
+        </nav>
+
+        <nav class="container center right">
+            <div class="menu">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#eventos">Eventos</a></li>
+                    <li><a href="#modalidades">Modalidades</a></li>
+                    <li><a href="#lojinha">Lojinha</a></li>
+                    <li><a href="#Hinos">Hinos</a></li>
+                    <li><a href="#parcerias">Parcerias</a></li>
+                </ul>
+            </div>
+            <div class="toggle open right"><i class="fas fa-bars"></i></div>
+            <div class="toggle close right"><i class="far fa-times-circle"></i></div>
+        </nav>
+    <div class="clear"></div>
+</header>
